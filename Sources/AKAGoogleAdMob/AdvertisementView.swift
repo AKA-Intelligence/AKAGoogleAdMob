33a7e1b8258929c587dcc47460d4599f5f5fd20a
UmaKimchi <uma@akaai.kr>
2023-01-19 17:38:42 +0900

    feat: update init for to add data stream

--- Sources/AKAGoogleAdMob/AdvertisementView.swift
+++ Sources/AKAGoogleAdMob/AdvertisementView.swift
@@ -1,21 +1,25 @@
 import GoogleMobileAds
 import SwiftUI
 import UIKit
+import Combine
 
 @available(iOS 13.0, *)
 public struct AdvertisementView: UIViewControllerRepresentable {
     public let id: String
+    public var showAd: AnyPublisher<Bool, Never>
     public let tapDismiss: () -> Void
     public init(
         for id: String,
+        showAd: AnyPublisher<Bool, Never>,
         tapDismiss: @escaping () -> Void
     ) {
         self.id = id
+        self.showAd = showAd
         self.tapDismiss = tapDismiss
     }
 
     public func makeUIViewController(context: Context) -> AdvertisementViewController {
-        let viewController = AdvertisementViewController(id)
+        let viewController = AdvertisementViewController(id, showAdPublisher: showAd)
         viewController.delegate = context.coordinator
         return viewController
     }
@@ -27,6 +31,7 @@ public struct AdvertisementView: UIViewControllerRepresentable {
     }
 
     final public class Coordinator: NSObject, AdvertisementViewControllerDelegate {
+        // MARK: - AdvertisementViewControllerDelegate
         public func adDidDismissFullScreenContent() {
             advertisementView.tapDismiss()
         }
@@ -43,15 +48,33 @@ public protocol AdvertisementViewControllerDelegate: AnyObject {
     func adDidDismissFullScreenContent()
 }
 
+@available(iOS 13.0, *)
 public class AdvertisementViewController: UIViewController {
 
     weak var delegate: AdvertisementViewControllerDelegate?
 
     private let id: String
-
-    init(_ id: String) {
+    private let showAdPublisher: AnyPublisher<Bool, Never>
+    private var cancellables: Set<AnyCancellable>
+    
+    @available(iOS 13.0, *)
+    init(_ id: String, showAdPublisher: AnyPublisher<Bool, Never>) {
         self.id = id
+        self.showAdPublisher = showAdPublisher
+        self.cancellables = .init()
         super.init(nibName: nil, bundle: nil)
+        
+        bind()
+    }
+    
+    private func bind() {
+        showAdPublisher
+            .sink {[weak self] show in
+                if show {
+                    self?.configureManager()
+                }
+            }
+            .store(in: &cancellables)
     }
 
     required init?(coder: NSCoder) {
@@ -60,7 +83,7 @@ public class AdvertisementViewController: UIViewController {
 
     public override func viewDidLoad() {
         super.viewDidLoad()
-        configureManager()
+//        configureManager()
     }
 
     private var interstitial: GADInterstitialAd?
@@ -83,6 +106,7 @@ public class AdvertisementViewController: UIViewController {
 }
 
 //MARK: - GADFullScreenContentDelegate
+@available(iOS 13.0, *)
 extension AdvertisementViewController: GADFullScreenContentDelegate {
     /// Tells the delegate that the ad failed to present full screen content.
     public func ad(_ ad: GADFullScreenPresentingAd, didFailToPresentFullScreenContentWithError error: Error) {
